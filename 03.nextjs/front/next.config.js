1ff71ec549c61ce04a829b270e3f3fc0b789cdd2
정석우 <ingal777@naver.com>
2022-09-17 22:18:30 +0900

    Feat:next.config.js에 withBundleAnalyzer 설치

--- 03.nextjs/front/next.config.js
+++ 03.nextjs/front/next.config.js
@@ -1,19 +1,20 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
 
 module.exports = withBundleAnalyzer({
-  distDir: '.next',
+  distDir: ".next",
+  compress: true, //build file 용량 압축
   webpack(config, { webpack }) {
-    const prod = process.env.NODE_ENV === 'production';
+    const prod = process.env.NODE_ENV === "production";
     const plugins = [
       ...config.plugins,
-      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
+      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/), //moment ko제외 모든언어 제거
     ];
     return {
       ...config,
-      mode: prod ? 'production' : 'development',
-      devtool: prod ? 'hidden-source-map' : 'eval',
+      mode: prod ? "production" : "development",
+      devtool: prod ? "hidden-source-map" : "eval",
       plugins,
     };
   },
